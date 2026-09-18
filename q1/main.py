51b0633b0e301f12462eed510f7178c65800e138
coarnado <coarnado@zrc.pshs.edu.ph>
2026-09-18 09:43:10 +0800

    M

--- q1/main.py
+++ q1/main.py
@@ -1,4 +1,12 @@
-print("zrc")
-print("this is my code")
-print("Potassium")
-print("formative")
+class Student:
+    def __init__(self, name):
+        self.name = name
+
+class Course:
+    def __init__(self, name):
+        self.name = name
+        self.students = []
+
+    def add_student(self, student):
+        self.students.append(student)
+
