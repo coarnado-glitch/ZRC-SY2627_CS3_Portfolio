5a09a2b89afcdb6847f0a006eb95da19335c2cd1
coarnado <coarnado@zrc.pshs.edu.ph>
2026-09-21 16:29:52 +0800

    m

--- q1/sg5/RPG-HeroClass.py
+++ q1/sg5/RPG-HeroClass.py
@@ -1,19 +1,61 @@
+<<<<<<< HEAD
+
+=======
 class Hero:
     def __init__(self, name, hp):
         self.name = name
         self.hp = hp
-
-    def take_damage(self, amount):
-        self.hp -= amount
+>>>>>>> bf59888124765648580e819e7e112f6e5f15709d
 
 
 
+
+<<<<<<< HEAD
+=======
 arthur = Hero("Arthur", 100)
 morgana = Hero("Morgana", 100)
+>>>>>>> bf59888124765648580e819e7e112f6e5f15709d
 
+<<<<<<< HEAD
+=======
 arthur.take_damage(10)
+>>>>>>> bf59888124765648580e819e7e112f6e5f15709d
 
 print(arthur.hp)    
 print(morgana.hp) 
 
+<<<<<<< HEAD
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=======
 #This is sodium :3
+>>>>>>> bf59888124765648580e819e7e112f6e5f15709d
